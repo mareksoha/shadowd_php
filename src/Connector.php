bb8957b900e5559715a122c74d1b367d17b883bb
mareks <20202245+mareksoha@users.noreply.github.com>
2022-06-01 10:00:01 +0200

    Update Connector.php

--- src/Connector.php
+++ src/Connector.php
@@ -55,7 +55,7 @@ class Connector
             $status = $connection->send($input);
             
             if ($status['attack'] === true) {
-                $output->error();
+                $output->error($exception);
             }
             
             if ($status['attack'] === false || $config->get('observe')) {
